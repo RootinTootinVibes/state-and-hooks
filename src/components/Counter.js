a19bbebdd6c76cc9b1c5ad9120e9e0c48dc9030b
artmoo2004 <artmoo2004@students.ecpi.edu>
2025-03-31 17:19:42 -0400

    Notes added for clarity of functions. Deleted unused logo.svg

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,14 +2,21 @@ import React, { useState } from 'react';
 
 const Button = () => {
     const [clicks, setClicks] = useState(0);
+
+    //Handles increasing displayed Counter
     const handleIncrement = () => {
         setClicks(clicks + 1);
     }
+
+    //Handles decreasing displayed Counter
+    //Count is also prevented from going below zero
     const handleDecrement = () => {
         if (clicks != 0) {
             setClicks(clicks - 1);
         }
     }
+
+    //Uses separate buttons to increase and decrease displayed count
     return (
         <div>
             <button onClick={handleIncrement}>Increase Count</button>
