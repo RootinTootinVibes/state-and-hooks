727a3b0898fb25dae96dd03d1e1b90c59fadc109
artmoo2004 <artmoo2004@students.ecpi.edu>
2025-03-31 17:29:25 -0400

    temp commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 const Button = () => {
     const [clicks, setClicks] = useState(0);
 
-<<<<<<< HEAD
     //Handles increasing displayed Counter
     const handleIncrement = () => {
         setClicks(clicks + 1);
@@ -14,27 +13,14 @@ const Button = () => {
     const handleDecrement = () => {
         if (clicks != 0) {
             setClicks(clicks - 1);
-=======
-    //handles both increase and decrease count
-    const handleChange = (direction) => {
-        
-        if (direction = "increase") {
-            //handling increase
-            setClicks(clicks + 1);
-        } else {
-            //handling decrease
-            if (clicks >= 0) {
-                setClicks(clicks - 1);
-            }
->>>>>>> d9c31ae8beabfc2b369c513ca971d3360630ffc4
         }
     }
 
     //Uses separate buttons to increase and decrease displayed count
     return (
         <div>
-            <button onClick={handleChange("increase")}>Increase Count</button>
-            <button onClick={handleChange("decrease")}>Decrease Count</button>
+            <button onClick={handleIncrement}>Increase Count</button>
+            <button onClick={handleDecrement}>Decrease Count</button>
             <p>Count Value: {clicks}</p>
         </div>
     );
